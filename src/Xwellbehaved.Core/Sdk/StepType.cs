5c6cc7ecf929d44c7de00709f72ba8b9640eff93
Michael W. Powell <mwpowelllde@gmail.com>
2020-12-06 14:52:13 -0500

    Added StepType to better differentiate between Scenario and supporting method invocations.

---
+++ src/Xwellbehaved.Core/Sdk/StepType.cs
@@ -0,0 +1,23 @@
+namespace Xwellbehaved.Sdk
+{
+    /// <summary>
+    /// Indicates the Type of Step being performed.
+    /// </summary>
+    public enum StepType
+    {
+        /// <summary>
+        /// A Background Step.
+        /// </summary>
+        Background,
+
+        /// <summary>
+        /// A Scenario Step.
+        /// </summary>
+        Scenario,
+
+        /// <summary>
+        /// A TearDown Step.
+        /// </summary>
+        TearDown
+    }
+}
