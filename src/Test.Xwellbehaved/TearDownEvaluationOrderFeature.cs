9edd3fad74e60b21019cfe6d597642a02af838d2
Michael W. Powell <mwpowelllde@gmail.com>
2020-12-06 12:23:22 -0500

    Added TearDownAttribute unit tests.

---
+++ src/Test.Xwellbehaved/TearDownEvaluationOrderFeature.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Collections.Generic;
+
+namespace Xwellbehaved
+{
+    using Infrastructure;
+    using Xunit;
+
+    public abstract class TearDownEvaluationOrderFeatureOne : Feature
+    {
+        protected Guid BaseOneId { get; } = Guid.NewGuid();
+
+        protected IList<Guid> Visited { get; } = new List<Guid>();
+
+        [TearDown]
+        public void TearDownOne()
+        {
+            "Visited should have 3 item".x(() => this.Visited.Count.AssertEqual(3));
+
+            "Then record teardown one visitation".x(() => this.Visited.Add(this.BaseOneId));
+        }
+    }
+
+    public abstract class TearDownEvaluationOrderFeatureTwo : TearDownEvaluationOrderFeatureOne
+    {
+        protected Guid BaseTwoId { get; } = Guid.NewGuid();
+
+        [TearDown]
+        public void TearDownTwo()
+        {
+            "Visited should have 2 item".x(() => this.Visited.Count.AssertEqual(2));
+
+            "Then record teardown two visitation".x(() => this.Visited.Add(this.BaseTwoId));
+        }
+    }
+
+    public abstract class TearDownEvaluationOrderFeatureThree : TearDownEvaluationOrderFeatureTwo
+    {
+        protected Guid BaseThreeId { get; } = Guid.NewGuid();
+
+        [TearDown]
+        public void TearDownThree()
+        {
+            "Visited should have 1 items".x(() => this.Visited.Count.AssertEqual(1));
+
+            "Record teardown three visitation".x(() => this.Visited.Add(this.BaseThreeId));
+        }
+    }
+
+    public abstract class TearDownEvaluationOrderFeatureFour : TearDownEvaluationOrderFeatureThree
+    {
+        protected Guid BaseFourId { get; } = Guid.NewGuid();
+
+        /// <summary>
+        /// Some of which should admitted be done during background. Also remembering that
+        /// the Scenario Background is like a stack in term of TearDown, the stack unwinds,
+        /// so we expect Visitation to occur in reverse order.
+        /// </summary>
+        [TearDown]
+        public void TearDownFour()
+        {
+            "Visited should not be null".x(() => this.Visited.AssertNotNull());
+
+            "And visited should not empty".x(() => this.Visited.AssertCollectionEmpty());
+
+            "Record teardown four visitation".x(() => this.Visited.Add(this.BaseFourId));
+        }
+    }
+
+    // #2 MWP 2020-07-01 01:08:55 PM / added a series of TearDown Evaluation Order features which verify correct evaluation order.
+    public class TearDownEvaluationOrderFeature : TearDownEvaluationOrderFeatureFour
+    {
+        /// <summary>
+        /// We verify TearDown method visitation this way by building up the Feature hierarchy
+        /// itself, rather than focusing on private subject classes. We need to do it this way in
+        /// order to maintain the Visited collection properly throughout the test resolution life
+        /// cycle.
+        /// </summary>
+        [Scenario]
+        public void TearDowns_should_be_visited_in_the_correct_order()
+        {
+
+#pragma warning disable IDE0022 // Use expression body for methods
+            // And we have early detection in the sense of TearDowns doing a little preliminary verification.
+            "Finally, Visited should appear in the expected order".x(() => this.Visited.AssertEqual(
+                new[] { this.BaseFourId, this.BaseThreeId, this.BaseTwoId, this.BaseOneId }));
+#pragma warning restore IDE0022 // Use expression body for methods
+
+        }
+    }
+}
