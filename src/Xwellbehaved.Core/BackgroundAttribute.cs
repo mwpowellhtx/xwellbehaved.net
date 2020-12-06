a20518bf28bd8fc73e384034f07aefedb555dca2
Michael W. Powell <mwpowelllde@gmail.com>
2020-12-06 12:19:27 -0500

    Added SupportMethodAttribute. Which informs both BackgroundAttribute as well as TearDownAttribute.

--- src/Xwellbehaved.Core/BackgroundAttribute.cs
+++ src/Xwellbehaved.Core/BackgroundAttribute.cs
@@ -1,20 +1,10 @@
-using System;
-using System.Diagnostics.CodeAnalysis;
-
 namespace Xwellbehaved
 {
     /// <summary>
     /// Applied to a method to indicate a background for each scenario defined in the same feature
     /// class.
     /// </summary>
-    [AttributeUsage(AttributeTargets.Method)
-        , SuppressMessage("Microsoft.Performance", "CA1813:AvoidUnsealedAttributes", Justification = "Designed for extensibility.")
-        , IgnoreXunitAnalyzersRule1013]
-    public class BackgroundAttribute : Attribute
+    public class BackgroundAttribute : SupportMethodAttribute
     {
-        [AttributeUsage(AttributeTargets.Class)]
-        private class IgnoreXunitAnalyzersRule1013Attribute : Attribute
-        {
-        }
     }
 }
---
+++ src/Xwellbehaved.Core/TearDownAttribute.cs
@@ -0,0 +1,10 @@
+namespace Xwellbehaved
+{
+    /// <summary>
+    /// Applied to a method to indicate a tear down for each scenario defined in the same feature
+    /// class.
+    /// </summary>
+    public class TearDownAttribute : SupportMethodAttribute
+    {
+    }
+}
