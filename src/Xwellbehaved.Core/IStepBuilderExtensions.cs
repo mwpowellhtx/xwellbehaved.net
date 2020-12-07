319fe25c81720324e89c6a0434ab2bd9a332b107
Michael W. Powell <mwpowelllde@gmail.com>
2020-12-07 14:32:42 -0500

    Remember to include the tear down extension methods as part of the Rollback migration path.

--- src/Xwellbehaved.Core/IStepBuilderExtensions.cs
+++ src/Xwellbehaved.Core/IStepBuilderExtensions.cs
@@ -15,45 +15,81 @@ namespace Xwellbehaved
         /// executed after all steps in the current scenario have been executed.
         /// </summary>
         /// <param name="stepBuilder">The step builder.</param>
-        /// <param name="action">The action.</param>
+        /// <param name="onTeardown">The teardown callback.</param>
         /// <returns>
         /// An instance of <see cref="IStepBuilder"/>.
         /// </returns>
-        public static IStepBuilder Teardown(this IStepBuilder stepBuilder, Action action) =>
-            action == null
-                ? stepBuilder
-                : stepBuilder.Teardown(context => action());
+        [Obsolete("We are moving away from tear down verbiage in favor of rollback so as not to confuse with Tear Down decoration")]
+        public static IStepBuilder Teardown(this IStepBuilder stepBuilder, Action onTeardown) => stepBuilder.Rollback(onTeardown);
 
         /// <summary>
         /// Declares a teardown action, relating to this step or previous steps, which will be
         /// executed after all steps in the current scenario have been executed.
         /// </summary>
         /// <param name="stepBuilder">The step builder.</param>
-        /// <param name="action">The action.</param>
+        /// <param name="onTeardown">The teardown callback.</param>
         /// <returns>
         /// An instance of <see cref="IStepBuilder"/>.
         /// </returns>
-        public static IStepBuilder Teardown(this IStepBuilder stepBuilder, Action<IStepContext> action) =>
-            action == null
+        [Obsolete("We are moving away from tear down verbiage in favor of rollback so as not to confuse with Tear Down decoration")]
+        public static IStepBuilder Teardown(this IStepBuilder stepBuilder, Action<IStepContext> onTeardown) => stepBuilder.Rollback(onTeardown);
+
+        /// <summary>
+        /// Declares a teardown action, relating to this step or previous steps, which will be
+        /// executed after all steps in the current scenario have been executed.
+        /// </summary>
+        /// <param name="stepBuilder">The step builder.</param>
+        /// <param name="onTeardown">The teardown callback.</param>
+        /// <returns>
+        /// An instance of <see cref="IStepBuilder"/>.
+        /// </returns>
+        [Obsolete("We are moving away from tear down verbiage in favor of rollback so as not to confuse with Tear Down decoration")]
+        public static IStepBuilder Teardown(this IStepBuilder stepBuilder, Func<Task> onTeardown) => stepBuilder.Rollback(onTeardown);
+
+        /// <summary>
+        /// Declares a rollback action, relating to this step or previous steps, which will be
+        /// executed after all steps in the current scenario have been executed.
+        /// </summary>
+        /// <param name="stepBuilder">The step builder.</param>
+        /// <param name="onRollback">The rollback callback.</param>
+        /// <returns>
+        /// An instance of <see cref="IStepBuilder"/>.
+        /// </returns>
+        public static IStepBuilder Rollback(this IStepBuilder stepBuilder, Action onRollback) =>
+            onRollback == null
                 ? stepBuilder
-                : stepBuilder?.Teardown(context =>
+                : stepBuilder.Rollback(context => onRollback());
+
+        /// <summary>
+        /// Declares a rollback action, relating to this step or previous steps, which will be
+        /// executed after all steps in the current scenario have been executed.
+        /// </summary>
+        /// <param name="stepBuilder">The step builder.</param>
+        /// <param name="onRollback">The rollback callback.</param>
+        /// <returns>
+        /// An instance of <see cref="IStepBuilder"/>.
+        /// </returns>
+        public static IStepBuilder Rollback(this IStepBuilder stepBuilder, Action<IStepContext> onRollback) =>
+            onRollback == null
+                ? stepBuilder
+                : stepBuilder?.Rollback(context =>
                 {
-                    action(context);
+                    onRollback(context);
                     return Task.FromResult(0);
                 });
 
         /// <summary>
-        /// Declares a teardown action, relating to this step or previous steps, which will be
+        /// Declares a rollback action, relating to this step or previous steps, which will be
         /// executed after all steps in the current scenario have been executed.
         /// </summary>
         /// <param name="stepBuilder">The step builder.</param>
-        /// <param name="action">The action.</param>
+        /// <param name="onRollback">The rollback callback.</param>
         /// <returns>
         /// An instance of <see cref="IStepBuilder"/>.
         /// </returns>
-        public static IStepBuilder Teardown(this IStepBuilder stepBuilder, Func<Task> action) =>
-            action == null
+        public static IStepBuilder Rollback(this IStepBuilder stepBuilder, Func<Task> onRollback) =>
+            onRollback == null
                 ? stepBuilder
-                : stepBuilder?.Teardown(context => action());
+                : stepBuilder?.Rollback(context => onRollback());
     }
 }
